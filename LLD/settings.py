02e84443c46eeb3da85f8a48454e69004b408855
dv <dv@locagen.com>
2024-02-04 21:06:18 +0100

    update allowed hosts

--- LLD/settings.py
+++ LLD/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-)^zoy57woq47f$30c7f36nk2vg=kt-19u$in*7$$yamw9h_j3x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vast-chamber-32914-4af41d6ec54e.herokuapp.com']
 
 
 # Application definition
